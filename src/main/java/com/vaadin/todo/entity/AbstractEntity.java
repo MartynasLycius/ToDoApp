e0dad3208a97ff3c7528dc347c68c5846b8670d7
Sudip Sarker <sudipdiu@gmail.com>
2020-05-23 19:08:02 +0600

    Remove * import

--- src/main/java/com/vaadin/todo/entity/AbstractEntity.java
+++ src/main/java/com/vaadin/todo/entity/AbstractEntity.java
@@ -1,6 +1,11 @@
 package com.vaadin.todo.entity;
 
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Id;
 
 @MappedSuperclass
 public abstract class AbstractEntity {
