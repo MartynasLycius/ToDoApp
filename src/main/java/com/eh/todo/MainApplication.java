af3dcfdf870b3e85b87e85c860d385c7c8f02f67
MD EMRAN HOSSAIN <emran@medisys.com.sa>
2020-10-27 23:37:37 +0600

    First commit for TODO Application

---
+++ src/main/java/com/eh/todo/MainApplication.java
@@ -0,0 +1,17 @@
+package com.eh.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Md. Emran Hossain <emranhos1@gmail.com>
+ * @version   1.0.00
+ * @since     1.0.00
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
