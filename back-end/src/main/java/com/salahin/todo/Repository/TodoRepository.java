28851131bbea6de5fad71cb74b0d1ebe1a7185e2
Salahin <salahin.dsi@bkash.com>
2021-01-23 16:21:58 +0600

    Create Todo repository and update git-ignore file

---
+++ back-end/src/main/java/com/salahin/todo/Repository/TodoRepository.java
@@ -0,0 +1,43 @@
+/**
+ * Created By: Md. Nazmus Salahin
+ * Created Date: 23-Jan-2021
+ * Time: 4:19 AM
+ * Modified By:
+ * Modified date:
+ * (C) CopyRight Salahin ltd.
+ */
+
+
+package com.salahin.todo.Repository;
+
+import com.salahin.todo.entities.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, UUID> {
+	
+	Optional<TodoEntity> findById(UUID id);
+	
+	@Query("select e from TodoEntity e")
+	List<TodoEntity> findAllToDo();
+	
+	@Modifying
+	@Query("UPDATE TodoEntity e SET e.name = :name, e.description = :description, e.date = :date where e.id = :id")
+	void updateTodoById(@Param("name") String name,
+	                    @Param("description") String description,
+	                    @Param("date") Date date,
+	                    @Param("id") UUID id);
+	
+	@Query("DELETE FROM TodoEntity e where e.id = ?1")
+	void deleteTodoById(UUID uuid);
+	
+}
