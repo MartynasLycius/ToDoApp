3366035e7c522c1723bd12d572716554abf7b97a
Salahin <salahin.dsi@bkash.com>
2021-01-24 20:47:24 +0600

    add cors origin filter and web configuration

---
+++ back-end/src/main/java/com/salahin/todo/configuration/WebConfig.java
@@ -0,0 +1,19 @@
+package com.salahin.todo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new MappingJackson2HttpMessageConverter());
+	}
+}
