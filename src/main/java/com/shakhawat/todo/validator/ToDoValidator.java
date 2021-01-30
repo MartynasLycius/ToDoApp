45082e75f3b3e6c9a68d1f7686825ef31d4d5c7e
Md. Shakhawat Hossain Mollah <sh.sumon99@gmail.com>
2021-01-30 22:37:43 +0600

    Validator update

--- src/main/java/com/shakhawat/todo/validator/ToDoValidator.java
+++ src/main/java/com/shakhawat/todo/validator/ToDoValidator.java
@@ -2,7 +2,6 @@ package com.shakhawat.todo.validator;
 
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.shakhawat.todo.model.ToDo;
@@ -19,14 +18,23 @@ public class ToDoValidator implements Validator {
     public void validate(Object o, Errors errors) {
     	ToDo toDo = (ToDo) o;
 
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "itemName", "This field is required");
+    	// Validation for Name of Task
+    	if (toDo.getItemName().trim().isEmpty()) {
+            errors.rejectValue("itemName", null, "Name of Task is required");
+        }
+    	
         if (toDo.getItemName().length() > 200) {
             errors.rejectValue("itemName", null, "Name of Task should be maximum 200 characters.");
         }
 
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "itemDescription", "This field is required");
+        // Validation for Description
+        if (toDo.getItemName().trim().isEmpty()) {
+            errors.rejectValue("itemDescription", null, "Description of Task is required");
+        }
+        
         if (toDo.getItemDescription().length() > 500) {
             errors.rejectValue("itemDescription", null, "Description of Task should be maximum 500 characters.");
         }
+        
     }
 }
