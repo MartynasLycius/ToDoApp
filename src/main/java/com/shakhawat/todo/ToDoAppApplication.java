bee24750380b90815a35c55db6922a42fbc87911
Md. Shakhawat Hossain Mollah <sh.sumon99@gmail.com>
2021-01-30 19:21:51 +0600

    Thymeleaf LayoutDialect bean initialization

--- src/main/java/com/shakhawat/todo/ToDoAppApplication.java
+++ src/main/java/com/shakhawat/todo/ToDoAppApplication.java
@@ -2,12 +2,20 @@ package com.shakhawat.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @SpringBootApplication
 public class ToDoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+	
+	@Bean
+	public LayoutDialect layoutDialect() {
+	    return new LayoutDialect();
 	}
 
 }
