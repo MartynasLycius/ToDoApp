6bae23f2974a9b12bbcf54ede83bd9b6dce6332c
dipanjal <dipanjal.maitra@bs-23.net>
2021-02-18 12:17:09 +0600

    init setup

---
+++ src/main/java/todo/proit/ProitTodoApplication.java
@@ -0,0 +1,13 @@
+package todo.proit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProitTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProitTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/todo/proit/ProitTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package todo.proit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProitTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
